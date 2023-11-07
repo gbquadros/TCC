444995a17e19a909f1fc7d2e447b5a452961d552
gbquadros <39174369+gbquadros@users.noreply.github.com>
2023-11-07 11:52:24 -0300

    Add files via upload

--- scratch.py
+++ scratch.py
@@ -1,13 +1,27 @@
 import docker
+from pymongo import MongoClient
+import psutil
+import os
+import json
+import paho.mqtt.client as mqtt
 
-print(f"Versão da biblioteca Docker SDK: {docker.__version__}")
+
+import paho.mqtt.publish as publish
+
+msgs = [{'topic': "cpu_usg", 'payload':""},
+        {'topic': "mem_usg", 'payload': ""}
+        #{'topic': "storage", 'payload' : ""}
+        ]
+
+host = "localhost"
+
 # Conecta-se ao Docker
 client = docker.from_env()
 
 # ID ou nome do container que você deseja monitorar
-container_id_or_name = "welcome-to-docker"
+container_id_or_name = "690e2581971257a0d59675ff5a6221aadc521c8e7cb75d62319471f66598de36"
 
-# Obtém informações do container
+#Obtém informações do container
 container = client.containers.get(container_id_or_name)
 
 # Obtém estatísticas de uso de recursos
@@ -17,19 +31,86 @@ stats = container.stats(stream=False)
 cpu_stats = stats['cpu_stats']
 memory_stats = stats['memory_stats']
 blkio_stats = stats['blkio_stats']
+disk_stats = stats['storage_stats']
+print ("blkio_stats: ", blkio_stats)
+
+print ("disk_stats: ", disk_stats)
 
 # Informações de CPU
 cpu_usage = cpu_stats['cpu_usage']['total_usage']
 system_cpu_usage = cpu_stats['system_cpu_usage']
 cpu_percent = (cpu_usage / system_cpu_usage) * 100
 
+
+
 # Informações de memória
 memory_usage = memory_stats['usage']
 memory_limit = memory_stats['limit']
 
+msgs = [{'topic': "cpu_usg", 'payload':cpu_percent},
+        {'topic': "mem_usg", 'payload': memory_usage}]
+
+if __name__ == '__main__':
+    # publish a single message
+    publish.single(topic='cpu_usg', payload=cpu_percent, hostname=host)
+    publish.single(topic='mem_usg', payload=memory_usage, hostname=host)
+
+# Obtenha o ID do processo principal do container
+pid = container.attrs['State']['Pid']
+
+# Obtenha o diretório raiz do processo do container no host
+#container_root_dir = os.path.join(os.environ['ProgramData'], 'docker', 'containers', container_id_or_name)
+
+# Use o psutil para obter as estatísticas de uso de disco
+#disk_usage = psutil.disk_usage(container_root_dir)
+
+# Calcule a porcentagem de uso de disco
+#disk_usage_percentage = (disk_usage.used / disk_usage.total) * 100
+
+#print(f'Uso de disco: {disk_usage_percentage:.2f}%')
+
 # Informações de disco
 io_service_bytes_recursive = blkio_stats['io_service_bytes_recursive']
+#used_bytes = disk_stats['bytes_used']
+#total_bytes = disk_stats['bytes_total']
 
-print(f"Uso de CPU: {cpu_percent}%")
-print(f"Uso de Memória: {memory_usage} bytes de {memory_limit} bytes")
-print(f"Uso de Disco: {io_service_bytes_recursive}")
+# Calcule a porcentagem de uso de disco
+#disk_usage_percentage = (used_bytes / total_bytes) * 100
+
+#print(f'Uso de disco : {disk_usage_percentage:.2f}%')
+
+#except docker.errors.NotFound:
+#    print(f'Container com ID {container_id} não encontrado.')
+
+#except Exception as e:
+#    print(f"Ocorreu um erro: {e}")
+
+
+try:
+    conn = MongoClient('localhost', 27017)
+    print("Connected successfully!!!") 
+except:   
+    print("Could not connect to MongoDB") 
+
+db = conn.tests
+
+collection = db.sensors
+
+
+#print(f"Uso de CPU: {cpu_percent}%")
+# print(f"Uso de Memória: {memory_usage} bytes de {memory_limit} bytes")
+# print(f"Uso de Disco: {io_service_bytes_recursive}")
+
+read1 = {
+   "Uso de CPU": cpu_percent,
+   "Uso de Memória": memory_usage,
+   #"Uso de Disco": disk_usage_percentage
+}
+
+insert1 = collection.insert_one(read1)
+
+
+#recuperar todos os registros ja inseridos
+cursor = collection.find() 
+for record in cursor: 
+    print(record)
--- subscriber.py
+++ subscriber.py
@@ -4,55 +4,43 @@
 A small example subscriber
 """
 import paho.mqtt.client as paho
-import psycopg2
+#import psycopg2
+from pymongo import MongoClient
 
-conn = psycopg2.connect(host="localhost", dbname="data_monitoring", user="postgres", password="123456",
-                        port="5432")
-cur = conn.cursor()
 
-#cur.execute("""CREATE TABLE IF NOT EXISTS data_collected(
-#    cpu_usage FLOAT,
-#    mem_usage FLOAT,
-#    disk_usage FLOAT
-#    );
-#""")
+try:
+    conn = MongoClient('localhost', 27017)
+    print("Connected successfully!!!")
+except:
+    print("Could not connect to MongoDB")
+
+db = conn.tests
+
+collection = db.sensors
+
+
 def on_message(mosq, obj, msg):
     print ("%-20s %s" % (msg.topic, msg.payload))
+
+
+    read1 = {
+        format(msg.payload.decode("utf-8"))
+    }
+
+    #print(msg.topic + " message payload is {}".format(msg.payload.decode("utf-8")))
+
+    print (read1)
+
+    insert1 = collection.insert_one(read1)
+
+    # recuperar todos os registros ja inseridos
+    cursor = collection.find()
+    for record in cursor:
+        print(record)
+
     topic = msg.topic
     curr_data = msg.payload
 
-    numbers = list(filter(lambda x: x.isdigit(), curr_data.split()))
-    result = [int(s) for s in numbers]
-    print (result)
-    if topic == 'cpu_usg':
-        curr_cpu = curr_data
-
-        #cur.execute("""INSERT INTO data_monitoring (cpu_usage) VALUES (curr_data)""")
-
-        #postgres_insert_query = """ INSERT INTO data_monitoring (cpu_usage) VALUES ("curr_data")"""
-        #record_to_insert = (curr_data)
-        #cur.execute(postgres_insert_query, record_to_insert)
-    if topic == 'mem_usg':
-        curr_mem = curr_data
-        #cur.execute("""INSERT INTO data_monitoring (mem_usage) VALUES (curr_data)""")
-    if topic == 'disk_usg':
-        curr_disk = curr_data
-        #cur.execute("""INSERT INTO data_monitoring (disk_usage) VALUES (curr_data)""")
-    #dados = msg.payload
-    #INSERT dados no tópico
-
-    #cur.execute("""INSERT INTO data_monitoring (cpu_usage, mem_usage, disk_usage)
-    #VALUES (curr_cpu, curr_mem, curr_disk)""")
-
-    #if (curr_mem != None and curr_disk != None and curr_cpu != None):
-    postgres_insert_query = """ INSERT INTO data_collected (cpu_usage, mem_usage, disk_usage) 
-    VALUES (1, 2, 3)"""
-    record_to_insert = (12.3, 14.8, 16.9)
-    cur.execute(""" INSERT INTO data_collected (cpu_usage, mem_usage, disk_usage) 
-    VALUES (1, 2, 3)""")
-    #curr_mem = None
-    #curr_cpu = None
-    #curr_disk = None
 
     mosq.publish('pong', 'ack', 0)
 
@@ -69,10 +57,11 @@ if __name__ == '__main__':
 
     client.subscribe("cpu_usg", 0)
     client.subscribe("mem_usg", 0)
-    client.subscribe("swap",0)
-    client.subscribe("storage", 0)
+    #client.subscribe("storage", 0)
 
     while client.loop() == 0:
         pass
+
+
 
 # vi: set fileencoding=utf-8 :
